16e351e194702783a54160e0256322970ce16499
Kevin Knight <57677197+kevin-m-knight-gs@users.noreply.github.com>
2021-05-03 17:15:55 -0400

    Refactor distributed serialization code and add tests (#194)
    * Add some distributed metadata tests
    
    * Refactor distributed metadata code

---
+++ legend-pure-runtime-java-engine-compiled/src/test/java/org/finos/legend/pure/runtime/java/compiled/serialization/binary/TestSimpleStringCaching.java
@@ -0,0 +1,50 @@
+// Copyright 2021 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.runtime.java.compiled.serialization.binary;
+
+import org.finos.legend.pure.m4.serialization.Reader;
+import org.finos.legend.pure.m4.serialization.Writer;
+import org.finos.legend.pure.runtime.java.compiled.serialization.GraphSerializer;
+import org.finos.legend.pure.runtime.java.compiled.serialization.model.Serialized;
+
+public class TestSimpleStringCaching extends TestStringCaching<SimpleStringCache>
+{
+    private static final String CACHE_PATH = "metadata/strings.idx";
+
+    @Override
+    protected SimpleStringCache buildCache()
+    {
+        Serialized serialized = GraphSerializer.serializeAll(repository.getTopLevels(), runtime.getProcessorSupport(), false);
+        return SimpleStringCache.fromSerialized(serialized);
+    }
+
+    @Override
+    protected void serialize(SimpleStringCache cache, FileWriter fileWriter)
+    {
+        try (Writer writer = fileWriter.getWriter(CACHE_PATH))
+        {
+            cache.write(writer);
+        }
+    }
+
+    @Override
+    protected StringIndex buildIndex(FileReader fileReader)
+    {
+        try (Reader reader = fileReader.getReader(CACHE_PATH))
+        {
+            return EagerStringIndex.fromReader(reader);
+        }
+    }
+}
