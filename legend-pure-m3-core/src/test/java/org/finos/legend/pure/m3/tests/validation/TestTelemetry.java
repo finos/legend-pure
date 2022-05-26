aed3ea53a9a475ee88b13a8c8e104b4e79497705
saisha95 <Saisha.LNU@gs.com>
2022-05-26 11:46:41 +0530

    Add new profile for Telemetry

---
+++ legend-pure-m3-core/src/test/java/org/finos/legend/pure/m3/tests/validation/TestTelemetry.java
@@ -0,0 +1,42 @@
+package org.finos.legend.pure.m3.tests.validation;
+
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.factory.Lists;
+import org.finos.legend.pure.m3.AbstractPureTestWithCoreCompiledPlatform;
+import org.finos.legend.pure.m3.serialization.filesystem.PureCodeStorage;
+import org.finos.legend.pure.m3.serialization.filesystem.repository.CodeRepository;
+import org.finos.legend.pure.m3.serialization.filesystem.repository.GenericCodeRepository;
+import org.finos.legend.pure.m3.serialization.filesystem.repository.PlatformCodeRepository;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestTelemetry extends AbstractPureTestWithCoreCompiledPlatform
+{
+        @BeforeClass
+        public static void setUp()
+        {
+            setUpRuntime(getFunctionExecution(), PureCodeStorage.createCodeStorage(getCodeStorageRoot(), getCodeRepositories()), getFactoryRegistryOverride(), getOptions(), getExtra());
+        }
+
+        protected static RichIterable<? extends CodeRepository> getCodeRepositories()
+        {
+            return Lists.immutable.with(CodeRepository.newPlatformCodeRepository(),
+                    GenericCodeRepository.build("system", "((meta)|(system)|(apps::pure))(::.*)?", PlatformCodeRepository.NAME),
+                    GenericCodeRepository.build("test", "test(::.*)?", PlatformCodeRepository.NAME, "system"));
+        }
+
+    @Test
+    public void testTelemetry()
+    {
+        compileTestSource("fromString.pure", "import meta::pure::profiles::*;\n" +
+                "\n" +
+                "function {telemetry.metricGroup = 'Testing telemetry tag'} pkg::func():String[1]\n" +
+                "{\n" +
+                "    let x = 'test';\n" +
+                "    $x;\n" +
+                "}\n" );
+
+        Assert.assertNotNull(runtime.getFunction("pkg::func():String[1]"));
+    }
+}
