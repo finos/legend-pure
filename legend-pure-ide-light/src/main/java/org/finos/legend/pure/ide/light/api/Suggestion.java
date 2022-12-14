9177072cd8240b836beba09fee21ce16f1432636
An Phi <aphi@skidmore.edu>
2022-12-14 12:22:08 -0500

    pure ide: support attribute suggestion

--- legend-pure-ide-light/src/main/java/org/finos/legend/pure/ide/light/api/Suggestion.java
+++ legend-pure-ide-light/src/main/java/org/finos/legend/pure/ide/light/api/Suggestion.java
@@ -7,9 +7,11 @@ import org.eclipse.collections.impl.factory.Lists;
 import org.finos.legend.pure.ide.light.helpers.response.ExceptionTranslation;
 import org.finos.legend.pure.ide.light.session.PureSession;
 import org.finos.legend.pure.m3.coreinstance.Package;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.extension.Profile;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.PackageableFunction;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.property.Property;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.type.Class;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.type.Enumeration;
 import org.finos.legend.pure.m3.navigation.M3Properties;
 import org.finos.legend.pure.m3.navigation.PackageableElement.PackageableElement;
 import org.finos.legend.pure.m3.navigation.ProcessorSupport;
@@ -27,6 +29,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Api(tags = "Suggestion")
@@ -85,7 +88,9 @@ public class Suggestion
             CoreInstance coreInstance = runtime.getCoreInstance(input.path);
             if (coreInstance instanceof Package)
             {
-                ListIterable<? extends CoreInstance> children = coreInstance.getValueForMetaPropertyToMany(M3Properties.children);
+                ListIterable<? extends CoreInstance> children = coreInstance.getValueForMetaPropertyToMany(M3Properties.children)
+                        .select(child -> input.types == null || input.types.isEmpty() || input.types.contains(child.getClassifier().getName()));
+
                 return Response.ok((StreamingOutput) outputStream ->
                 {
                     outputStream.write("[".getBytes());
@@ -94,6 +99,16 @@ public class Suggestion
                         CoreInstance child = children.get(i);
                         String pureName = child instanceof PackageableFunction ? child.getValueForMetaPropertyToOne(M3Properties.functionName).getName() : child.getValueForMetaPropertyToOne(M3Properties.name).getName();
                         String text = child instanceof PackageableFunction ? Function.prettyPrint(child, processorSupport) : child.getValueForMetaPropertyToOne(M3Properties.name).getName();
+                        MutableList<Property> requiredClassProperties = child instanceof Class ? org.finos.legend.pure.m3.navigation.property.Property.getAllProperties(child, processorSupport)
+                                // NOTE: make sure to only consider required (non-qualified) properties: i.e. multiplicity lower bound != 0
+                                .selectInstancesOf(Property.class).select(prop ->
+                                        {
+                                            CoreInstance lowerBound = prop.getValueForMetaPropertyToOne(M3Properties.multiplicity).getValueForMetaPropertyToOne(M3Properties.lowerBound);
+                                            // NOTE: here the lower bound can be nullish when there's multiplicity parameter being used
+                                            // but we skip that case for now
+                                            return lowerBound != null && !lowerBound.getValueForMetaPropertyToOne(M3Properties.value).getName().equals("0");
+                                        }
+                                ).toList() : Lists.mutable.empty();
 
                         outputStream.write("{\"pureType\":\"".getBytes());
                         outputStream.write(JSONValue.escape(child.getClassifier().getName()).getBytes());
@@ -103,7 +118,28 @@ public class Suggestion
                         outputStream.write(JSONValue.escape(PackageableElement.getUserPathForPackageableElement(child)).getBytes());
                         outputStream.write("\",\"text\":\"".getBytes());
                         outputStream.write(JSONValue.escape(text).getBytes());
-                        outputStream.write("\"}".getBytes());
+                        outputStream.write("\"".getBytes());
+
+                        if (requiredClassProperties.notEmpty())
+                        {
+                            outputStream.write(",\"requiredClassProperties\":[".getBytes());
+
+                            for (int j = 0; j < requiredClassProperties.size(); j++)
+                            {
+                                outputStream.write("\"".getBytes());
+                                outputStream.write(JSONValue.escape(requiredClassProperties.get(j).getValueForMetaPropertyToOne(M3Properties.name).getName()).getBytes());
+                                outputStream.write("\"".getBytes());
+
+                                if (j != requiredClassProperties.size() - 1)
+                                {
+                                    outputStream.write(",".getBytes());
+                                }
+                            }
+
+                            outputStream.write("]".getBytes());
+                        }
+
+                        outputStream.write("}".getBytes());
 
                         if (i != children.size() - 1)
                         {
@@ -133,6 +169,7 @@ public class Suggestion
     public static class IncompletePathSuggestionInput
     {
         public String path;
+        public List<String> types;
     }
 
     @POST
@@ -163,6 +200,16 @@ public class Suggestion
                     CoreInstance child = children.get(i);
                     String pureName = child instanceof PackageableFunction ? child.getValueForMetaPropertyToOne(M3Properties.functionName).getName() : child.getValueForMetaPropertyToOne(M3Properties.name).getName();
                     String text = child instanceof PackageableFunction ? Function.prettyPrint(child, processorSupport) : child.getValueForMetaPropertyToOne(M3Properties.name).getName();
+                    MutableList<Property> requiredClassProperties = child instanceof Class ? org.finos.legend.pure.m3.navigation.property.Property.getAllProperties(child, processorSupport)
+                            // NOTE: make sure to only consider required (non-qualified) properties: i.e. multiplicity lower bound != 0
+                            .selectInstancesOf(Property.class).select(prop ->
+                                    {
+                                        CoreInstance lowerBound = prop.getValueForMetaPropertyToOne(M3Properties.multiplicity).getValueForMetaPropertyToOne(M3Properties.lowerBound);
+                                        // NOTE: here the lower bound can be nullish when there's multiplicity parameter being used
+                                        // but we skip that case for now
+                                        return lowerBound != null && !lowerBound.getValueForMetaPropertyToOne(M3Properties.value).getName().equals("0");
+                                    }
+                            ).toList() : Lists.mutable.empty();
 
                     outputStream.write("{\"pureType\":\"".getBytes());
                     outputStream.write(JSONValue.escape(child.getClassifier().getName()).getBytes());
@@ -172,7 +219,28 @@ public class Suggestion
                     outputStream.write(JSONValue.escape(PackageableElement.getUserPathForPackageableElement(child)).getBytes());
                     outputStream.write("\",\"text\":\"".getBytes());
                     outputStream.write(JSONValue.escape(text).getBytes());
-                    outputStream.write("\"}".getBytes());
+                    outputStream.write("\"".getBytes());
+
+                    if (requiredClassProperties.notEmpty())
+                    {
+                        outputStream.write(",\"requiredClassProperties\":[".getBytes());
+
+                        for (int j = 0; j < requiredClassProperties.size(); j++)
+                        {
+                            outputStream.write("\"".getBytes());
+                            outputStream.write(JSONValue.escape(requiredClassProperties.get(j).getValueForMetaPropertyToOne(M3Properties.name).getName()).getBytes());
+                            outputStream.write("\"".getBytes());
+
+                            if (j != requiredClassProperties.size() - 1)
+                            {
+                                outputStream.write(",".getBytes());
+                            }
+                        }
+
+                        outputStream.write("]".getBytes());
+                    }
+
+                    outputStream.write("}".getBytes());
 
                     if (i != children.size() - 1)
                     {
@@ -200,14 +268,126 @@ public class Suggestion
     }
 
     @POST
-    @Path("suggestion/class")
+    @Path("suggestion/attribute")
     public Response getSuggestionsForAttribute(@Context HttpServletRequest request,
-                                               ClassSuggestionInput input,
+                                               AttributeSuggestionInput input,
                                                @Context HttpServletResponse response)
     {
         PureRuntime runtime = this.session.getPureRuntime();
         ProcessorSupport processorSupport = runtime.getProcessorSupport();
-        MutableList<String> packagePaths = input.packagePath != null ? Lists.mutable.of(input.packagePath) : Lists.adapt(input.importPaths).withAll(AUTO_IMPORTS).distinct();
+        // NOTE: here we take into account: first, the imported packages in scope, then the root package (::) and lastly
+        // the auto imported packages in the global scope
+        MutableList<String> allPackagePaths = Lists.adapt(input.importPaths).withAll(AUTO_IMPORTS).distinct();
+        MutableList<String> paths = input.path.contains("::") ? Lists.mutable.of(input.path) : allPackagePaths.collect(pkg -> pkg.concat("::" + input.path));
+
+        try
+        {
+            MutableList<AttributeSuggestion> suggestions = paths.collect(runtime::getCoreInstance)
+                    // These are the sensible elements to get attributes from at the moment
+                    .select(el -> el instanceof Class || el instanceof Enumeration || el instanceof Profile)
+                    .flatCollect(el ->
+                    {
+                        if (el instanceof Class)
+                        {
+                            return org.finos.legend.pure.m3.navigation.property.Property.getAllProperties(el, processorSupport).collect(property -> new AttributeSuggestion(
+                                    property.getClassifier().getName(),
+                                    property.getValueForMetaPropertyToOne(M3Properties.name).getName(),
+                                    el.getClassifier().getName(),
+                                    PackageableElement.getUserPathForPackageableElement(el)
+                            ));
+                        }
+                        if (el instanceof Profile)
+                        {
+                            return Lists.mutable.withAll(el.getValueForMetaPropertyToMany(M3Properties.p_tags).collect(tag -> new AttributeSuggestion(
+                                    tag.getClassifier().getName(),
+                                    tag.getValueForMetaPropertyToOne(M3Properties.value).getName(),
+                                    el.getClassifier().getName(),
+                                    PackageableElement.getUserPathForPackageableElement(el)
+                            ))).withAll(el.getValueForMetaPropertyToMany(M3Properties.p_stereotypes).collect(stereotype -> new AttributeSuggestion(
+                                    stereotype.getClassifier().getName(),
+                                    stereotype.getValueForMetaPropertyToOne(M3Properties.value).getName(),
+                                    el.getClassifier().getName(),
+                                    PackageableElement.getUserPathForPackageableElement(el)))
+                            );
+                        }
+                        if (el instanceof Enumeration)
+                        {
+                            return el.getValueForMetaPropertyToMany(M3Properties.values).collect(enumValue -> new AttributeSuggestion(
+                                    enumValue.getClassifier().getName(),
+                                    enumValue.getValueForMetaPropertyToOne(M3Properties.name).getName(),
+                                    el.getClassifier().getName(),
+                                    PackageableElement.getUserPathForPackageableElement(el)
+                            ));
+                        }
+                        return Collections.emptyList();
+                    });
+            return Response.ok((StreamingOutput) outputStream ->
+            {
+                outputStream.write("[".getBytes());
+                for (int i = 0; i < suggestions.size(); i++)
+                {
+                    AttributeSuggestion suggestion = suggestions.get(i);
+
+                    outputStream.write("{\"pureType\":\"".getBytes());
+                    outputStream.write(JSONValue.escape(suggestion.pureType).getBytes());
+                    outputStream.write("\",\"pureName\":\"".getBytes());
+                    outputStream.write(JSONValue.escape(suggestion.pureName).getBytes());
+                    outputStream.write("\",\"owner\":\"".getBytes());
+                    outputStream.write(JSONValue.escape(suggestion.owner).getBytes());
+                    outputStream.write("\",\"ownerPureType\":\"".getBytes());
+                    outputStream.write(JSONValue.escape(suggestion.ownerPureType).getBytes());
+                    outputStream.write("\"}".getBytes());
+
+                    if (i != suggestions.size() - 1)
+                    {
+                        outputStream.write(",".getBytes());
+                    }
+                }
+                outputStream.write("]".getBytes());
+                outputStream.close();
+            }).build();
+        }
+        catch (Exception e)
+        {
+            return Response.status(Response.Status.BAD_REQUEST).entity((StreamingOutput) outputStream ->
+            {
+                outputStream.write(("\"" + JSONValue.escape(ExceptionTranslation.buildExceptionMessage(session, e, new ByteArrayOutputStream()).getText()) + "\"").getBytes());
+                outputStream.close();
+            }).build();
+        }
+    }
+
+    private static class AttributeSuggestion
+    {
+        public final String pureType;
+        public final String pureName;
+        public final String ownerPureType;
+        public final String owner;
+
+        public AttributeSuggestion(String pureType, String pureName, String ownerPureType, String owner)
+        {
+            this.pureType = pureType;
+            this.pureName = pureName;
+            this.ownerPureType = ownerPureType;
+            this.owner = owner;
+        }
+    }
+
+    public static class AttributeSuggestionInput
+    {
+        public List<String> importPaths;
+        public String path;
+    }
+
+    @POST
+    @Path("suggestion/class")
+    public Response getSuggestionsForClass(@Context HttpServletRequest request,
+                                           ClassSuggestionInput input,
+                                           @Context HttpServletResponse response)
+    {
+        PureRuntime runtime = this.session.getPureRuntime();
+        ProcessorSupport processorSupport = runtime.getProcessorSupport();
+        MutableList<String> packagePaths = Lists.adapt(input.importPaths).withAll(AUTO_IMPORTS).distinct();
 
         try
         {
@@ -220,8 +400,7 @@ public class Suggestion
                 for (int i = 0; i < classes.size(); i++)
                 {
                     Class<?> cls = classes.get(i);
-
-                    MutableList<Property> requiredProperties = org.finos.legend.pure.m3.navigation.property.Property.getAllProperties(cls, processorSupport)
+                    MutableList<Property> requiredClassProperties = org.finos.legend.pure.m3.navigation.property.Property.getAllProperties(cls, processorSupport)
                             // NOTE: make sure to only consider required (non-qualified) properties: i.e. multiplicity lower bound != 0
                             .selectInstancesOf(Property.class).select(prop ->
                                     {
@@ -236,15 +415,15 @@ public class Suggestion
                     outputStream.write(JSONValue.escape(cls.getValueForMetaPropertyToOne(M3Properties.name).getName()).getBytes());
                     outputStream.write("\",\"pureId\":\"".getBytes());
                     outputStream.write(JSONValue.escape(PackageableElement.getUserPathForPackageableElement(cls)).getBytes());
-                    outputStream.write("\",\"requiredProperties\":[".getBytes());
+                    outputStream.write("\",\"requiredClassProperties\":[".getBytes());
 
-                    for (int j = 0; j < requiredProperties.size(); j++)
+                    for (int j = 0; j < requiredClassProperties.size(); j++)
                     {
                         outputStream.write("\"".getBytes());
-                        outputStream.write(JSONValue.escape(requiredProperties.get(j).getValueForMetaPropertyToOne(M3Properties.name).getName()).getBytes());
+                        outputStream.write(JSONValue.escape(requiredClassProperties.get(j).getValueForMetaPropertyToOne(M3Properties.name).getName()).getBytes());
                         outputStream.write("\"".getBytes());
 
-                        if (j != requiredProperties.size() - 1)
+                        if (j != requiredClassProperties.size() - 1)
                         {
                             outputStream.write(",".getBytes());
                         }
@@ -275,6 +454,5 @@ public class Suggestion
     public static class ClassSuggestionInput
     {
         public List<String> importPaths;
-        public String packagePath;
     }
 }
