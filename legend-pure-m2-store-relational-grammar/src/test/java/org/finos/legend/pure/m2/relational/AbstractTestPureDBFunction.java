bbd620903fb74bfa994039f237d09497239dcf83
Kevin Knight <57677197+kevin-m-knight-gs@users.noreply.github.com>
2023-02-17 18:10:28 -0500

    Fix tests so they do not depend on a particular H2 version (#661)

--- legend-pure-m2-store-relational-grammar/src/test/java/org/finos/legend/pure/m2/relational/AbstractTestPureDBFunction.java
+++ legend-pure-m2-store-relational-grammar/src/test/java/org/finos/legend/pure/m2/relational/AbstractTestPureDBFunction.java
@@ -14,10 +14,12 @@
 
 package org.finos.legend.pure.m2.relational;
 
-import org.finos.legend.pure.m3.tests.AbstractPureTestWithCoreCompiled;
 import org.finos.legend.pure.m3.exception.PureExecutionException;
+import org.finos.legend.pure.m3.tests.AbstractPureTestWithCoreCompiled;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.regex.Pattern;
 
 public abstract class AbstractTestPureDBFunction extends AbstractPureTestWithCoreCompiled
 {
@@ -40,8 +42,8 @@ public abstract class AbstractTestPureDBFunction extends AbstractPureTestWithCor
                         "Database mydb()\n"
         );
         PureExecutionException e = Assert.assertThrows(PureExecutionException.class, () -> compileAndExecute("test():Any[0..1]"));
-        assertPureException(PureExecutionException.class, "Error executing sql query; SQL reason: Syntax error in SQL statement \"CREATE LOCAL TEMPORARY TABLE ([*]COL INT)\"; expected \"identifier\"; SQL statement:\n" +
-                "Create LOCAL TEMPORARY TABLE (col INT) [42001-200]; SQL error code: 42001; SQL state: 42001", 8, 4, e);
+        assertPureException(PureExecutionException.class, Pattern.compile("Error executing sql query; SQL reason: Syntax error in SQL statement \"CREATE LOCAL TEMPORARY TABLE \\(\\[\\*]COL INT\\) ?\"; expected \"identifier\"; SQL statement:\n" +
+                "Create LOCAL TEMPORARY TABLE \\(col INT\\) \\[42001-\\d++]; SQL error code: 42001; SQL state: 42001"), 8, 4, e);
     }
 
     @Test
@@ -61,8 +63,8 @@ public abstract class AbstractTestPureDBFunction extends AbstractPureTestWithCor
                         "Database mydb()\n"
         );
         PureExecutionException e = Assert.assertThrows(PureExecutionException.class, () -> compileAndExecute("test():Any[0..1]"));
-        assertPureException(PureExecutionException.class, "Error executing sql query; SQL reason: Table \"TT\" not found; SQL statement:\n" +
-                "drop table tt [42102-200]; SQL error code: 42102; SQL state: 42S02", 8, 4, e);
+        assertPureException(PureExecutionException.class, Pattern.compile("Error executing sql query; SQL reason: Table \"TT\" not found; SQL statement:\n" +
+                "drop table tt \\[42102-\\d++]; SQL error code: 42102; SQL state: 42S02"), 8, 4, e);
     }
 
     @Test
@@ -82,7 +84,7 @@ public abstract class AbstractTestPureDBFunction extends AbstractPureTestWithCor
                         "Database mydb()\n"
         );
         PureExecutionException e = Assert.assertThrows(PureExecutionException.class, () -> compileAndExecute("test():Any[0..1]"));
-        assertPureException(PureExecutionException.class, "Error executing sql query; SQL reason: Table \"TT\" not found; SQL statement:\n" +
-                "select * from tt [42102-200]; SQL error code: 42102; SQL state: 42S02", 8, 4, e);
+        assertPureException(PureExecutionException.class, Pattern.compile("Error executing sql query; SQL reason: Table \"TT\" not found; SQL statement:\n" +
+                "select \\* from tt \\[42102-\\d++]; SQL error code: 42102; SQL state: 42S02"), 8, 4, e);
     }
 }
