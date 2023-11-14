a90b1ae3870cb7b3173aa944bbf99d78f33647af
John Pentelow <John.Pentelow@gs.com>
2023-11-14 13:10:15 +0000

    Fix indexOf and char in compiled eval flow

---
+++ legend-pure-m3-core/src/test/java/org/finos/legend/pure/m3/tests/function/base/string/AbstractTestChar.java
@@ -0,0 +1,43 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.m3.tests.function.base.string;
+
+import org.finos.legend.pure.m3.tests.AbstractPureTestWithCoreCompiled;
+import org.junit.Test;
+
+public class AbstractTestChar extends AbstractPureTestWithCoreCompiled
+{
+    @Test
+    public void testBasicParse()
+    {
+        compileTestSource("fromString.pure",
+                "function test():Boolean[1]\n" +
+                        "{\n" +
+                        "   assert('a' == char(97), |'');\n" +
+                        "}\n");
+        this.execute("test():Boolean[1]");
+    }
+
+    @Test
+    public void testEvalParse()
+    {
+        compileTestSource("fromString.pure",
+                "function test():Boolean[1]\n" +
+                        "{\n" +
+                        "   assert('a' == char_Integer_1__String_1_->eval(97), |'');\n" +
+                        "}\n");
+        this.execute("test():Boolean[1]");
+    }
+}
--- legend-pure-runtime-java-extension-functions/src/main/java/org/finos/legend/pure/runtime/java/extension/functions/compiled/natives/string/IndexOf.java
+++ legend-pure-runtime-java-extension-functions/src/main/java/org/finos/legend/pure/runtime/java/extension/functions/compiled/natives/string/IndexOf.java
@@ -14,36 +14,13 @@
 
 package org.finos.legend.pure.runtime.java.extension.functions.compiled.natives.string;
 
-import org.eclipse.collections.api.list.ListIterable;
-import org.finos.legend.pure.m4.coreinstance.CoreInstance;
-import org.finos.legend.pure.runtime.java.compiled.generation.ProcessorContext;
-import org.finos.legend.pure.runtime.java.compiled.generation.processors.natives.AbstractNative;
+import org.finos.legend.pure.runtime.java.compiled.generation.processors.natives.AbstractNativeFunctionGeneric;
+import org.finos.legend.pure.runtime.java.compiled.generation.processors.support.CompiledSupport;
 
-public class IndexOf extends AbstractNative
+public class IndexOf extends AbstractNativeFunctionGeneric
 {
     public IndexOf()
     {
-        super("indexOf_String_1__String_1__Integer_1_", "indexOf_String_1__String_1__Integer_1__Integer_1_");
-    }
-
-    @Override
-    public String build(CoreInstance topLevelElement, CoreInstance functionExpression, ListIterable<String> transformedParams, ProcessorContext processorContext)
-    {
-        String fromIndex = transformedParams.size() == 3 ? ",((Long)" + transformedParams.get(2) + ").intValue()" : "";
-        return "Long.valueOf(((String)" + transformedParams.get(0) + ")" +
-                ".indexOf((String)" + transformedParams.get(1) + fromIndex + "))";
-    }
-
-    @Override
-    public String buildBody()
-    {
-        return "new DefendedPureFunction3<String, String, Number, Long>()\n" +
-                "        {\n" +
-                "            @Override\n" +
-                "            public Long value(String input, String str, Number from, ExecutionSupport es)\n" +
-                "            {\n" +
-                "               return from == null ? Long.valueOf(input.indexOf(str)) : Long.valueOf(input.indexOf(str, from.intValue()));" +
-                "            }\n" +
-                "        }";
+        super(getMethod(CompiledSupport.class, "indexOf", String.class, String.class),"indexOf_String_1__String_1__Integer_1_");
     }
 }
