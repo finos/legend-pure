43231207e88e4858fce9bac584a10202357c79c7
kevin-m-knight-gs <kevin.knight@gs.com>
2025-03-14 11:42:30 -0400

    Move TestMatch back from legend-engine

---
+++ legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/test/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/support/function/base/lang/TestMatch.java
@@ -0,0 +1,67 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.runtime.java.compiled.generation.processors.support.function.base.lang;
+
+import org.finos.legend.pure.m3.execution.FunctionExecution;
+import org.finos.legend.pure.m3.tests.function.base.lang.AbstractTestMatch;
+import org.finos.legend.pure.runtime.java.compiled.execution.FunctionExecutionCompiledBuilder;
+import org.finos.legend.pure.runtime.java.compiled.factory.JavaModelFactoryRegistryLoader;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestMatch extends AbstractTestMatch
+{
+    @BeforeClass
+    public static void setUp()
+    {
+        setUpRuntime(getFunctionExecution(), JavaModelFactoryRegistryLoader.loader());
+    }
+
+    protected static FunctionExecution getFunctionExecution()
+    {
+        return new FunctionExecutionCompiledBuilder().build();
+    }
+
+    @Test
+    public void testUnAssignedMatchInFuncExpression()
+    {
+        compileSource(
+                "fromString.pure",
+                "function testMatch(v:Any[1]):String[1]\n" +
+                        "{\n" +
+                        "   $v->match(  [\n" +
+                        "                    a:AA[1] | 'AA',\n" +
+                        "                    b:BB[1] | 'BB'\n" +
+                        "                ]\n" +
+                        "             );\n" +
+                        "\n" +
+                        "   $v->match(  [\n" +
+                        "                    a:AA[1] | 'AA',\n" +
+                        "                    b:BB[1] | 'BB'\n" +
+                        "                ]\n" +
+                        "             );\n" +
+                        "}\n" +
+                        "\n" +
+                        "Class AA\n" +
+                        "{\n" +
+                        "   name:String[*];\n" +
+                        "}\n" +
+                        "\n" +
+                        "Class BB\n" +
+                        "{\n" +
+                        "   name:String[*];\n" +
+                        "}\n");
+    }
+}
