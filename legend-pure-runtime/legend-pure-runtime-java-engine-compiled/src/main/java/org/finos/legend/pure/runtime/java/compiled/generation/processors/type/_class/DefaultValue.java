132e3e3f2fbc9bb05b1ecd9d6c6f535dea5b5b2e
kevin-m-knight-gs <kevin.knight@gs.com>
2025-07-17 16:54:28 -0400

    Improve default value generation in compiled mode

--- legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/main/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/type/_class/DefaultValue.java
+++ legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/main/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/type/_class/DefaultValue.java
@@ -17,14 +17,16 @@ package org.finos.legend.pure.runtime.java.compiled.generation.processors.type._
 import org.eclipse.collections.api.RichIterable;
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ListIterable;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.tuple.Pair;
+import org.eclipse.collections.impl.tuple.Tuples;
 import org.finos.legend.pure.m3.navigation.Instance;
 import org.finos.legend.pure.m3.navigation.M3Properties;
-import org.finos.legend.pure.m3.navigation.PackageableElement.PackageableElement;
 import org.finos.legend.pure.m3.navigation.ProcessorSupport;
-import org.finos.legend.pure.m3.navigation._class._Class;
 import org.finos.legend.pure.m3.navigation.multiplicity.Multiplicity;
 import org.finos.legend.pure.m3.navigation.property.Property;
 import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+import org.finos.legend.pure.runtime.java.compiled.generation.JavaPackageAndImportBuilder;
 import org.finos.legend.pure.runtime.java.compiled.generation.ProcessorContext;
 import org.finos.legend.pure.runtime.java.compiled.generation.processors.valuespecification.ValueSpecificationProcessor;
 
@@ -39,32 +41,44 @@ public class DefaultValue
 
     public static ListIterable<String> manageDefaultValues(BiFunction<String, String, String> formatString, CoreInstance sourceClass, boolean doSingleWrap, boolean includeInheritedProperties, ProcessorContext processorContext)
     {
+        RichIterable<? extends CoreInstance> properties = includeInheritedProperties ? processorContext.getSupport().class_getSimpleProperties(sourceClass) : sourceClass.getValueForMetaPropertyToMany(M3Properties.properties);
+        MutableList<Pair<String, String>> result = Lists.mutable.empty();
+        properties.forEach(p ->
+        {
+            String javaExpression = getDefaultValueJavaExpression(p, doSingleWrap, processorContext);
+            if (javaExpression != null)
+            {
+                result.add(Tuples.pair(p.getName(), javaExpression));
+            }
+        });
+        return result.sortThisBy(Pair::getOne).collect(p -> formatString.apply(p.getOne(), p.getTwo()));
+    }
+
+    public static String getDefaultValueJavaExpression(CoreInstance property, boolean doSingleWrap, ProcessorContext processorContext)
+    {
+        CoreInstance defaultValue = property.getValueForMetaPropertyToOne(M3Properties.defaultValue);
+        if (defaultValue == null)
+        {
+            return null;
+        }
+
         ProcessorSupport processorSupport = processorContext.getSupport();
-        RichIterable<? extends CoreInstance> properties = includeInheritedProperties ? _Class.getSimpleProperties(sourceClass, processorSupport) : sourceClass.getValueForMetaPropertyToMany(M3Properties.properties);
+        CoreInstance expression = Property.getDefaultValueExpression(defaultValue);
+        String value = ValueSpecificationProcessor.processValueSpecification(expression, processorContext);
+        if ("this".equals(value))
+        {
+            CoreInstance expressionRawType = Instance.getValueForMetaPropertyToOneResolved(expression, M3Properties.genericType, org.finos.legend.pure.m3.navigation.M3Properties.rawType, processorSupport);
+            value = JavaPackageAndImportBuilder.buildImplClassNameFromType(expressionRawType, processorContext.getClassImplSuffix(), processorSupport) + ".this";
+        }
 
-        return properties.collectIf(
-                p -> p.getValueForMetaPropertyToOne(M3Properties.defaultValue) != null,
-                p ->
-                {
-                    boolean propertyIsToOne = Multiplicity.isToOne(Instance.getValueForMetaPropertyToOneResolved(p, M3Properties.multiplicity, processorSupport), false);
-                    CoreInstance expression = Property.getDefaultValueExpression(Instance.getValueForMetaPropertyToOneResolved(p, M3Properties.defaultValue, processorSupport));
-                    String value = ValueSpecificationProcessor.processValueSpecification(expression, processorContext);
-                    if ("this".equals(value))
-                    {
-                        CoreInstance expressionRawType = Instance.getValueForMetaPropertyToOneResolved(Instance.getValueForMetaPropertyToOneResolved(expression, M3Properties.genericType, processorSupport), M3Properties.rawType, processorSupport);
-                        value = PackageableElement.getSystemPathForPackageableElement(expressionRawType, "_") + processorContext.getClassImplSuffix() + "." + value;
-                    }
+        boolean propertyIsToOne = Multiplicity.isToOne(Instance.getValueForMetaPropertyToOneResolved(property, M3Properties.multiplicity, processorSupport), false);
+        CoreInstance expressionMultiplicity = Instance.getValueForMetaPropertyToOneResolved(expression, M3Properties.multiplicity, processorSupport);
+        if ((doSingleWrap || !propertyIsToOne) && (Multiplicity.isLowerZero(expressionMultiplicity) || Multiplicity.isToOne(expressionMultiplicity)))
+        {
+            //wrap
+            value = "CompiledSupport.toPureCollection(" + value + ")";
+        }
 
-                    CoreInstance expressionMultiplicity = Multiplicity.newMultiplicity(expression.getValueForMetaPropertyToMany(M3Properties.values).size(), processorSupport);
-
-                    if ((doSingleWrap || !propertyIsToOne) && (Multiplicity.isLowerZero(expressionMultiplicity) || Multiplicity.isToOne(expressionMultiplicity)))
-                    {
-                        //wrap
-                        value = "CompiledSupport.toPureCollection(" + value + ")";
-                    }
-
-                    return formatString.apply(p.getName(), value);
-                },
-                Lists.mutable.empty());
+        return value;
     }
 }
