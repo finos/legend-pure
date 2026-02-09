29b6cbe1dd5f5557e8e20ffa078a109732085ae4
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2026-02-09 15:08:38 -0500

    feat: Add TestColSpecAnnotations

---
+++ legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/test/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/support/function/base/relation/TestColSpecAnnotations.java
@@ -0,0 +1,60 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.runtime.java.compiled.generation.processors.support.function.base.relation;
+
+import org.finos.legend.pure.m3.tests.AbstractPureTestWithCoreCompiled;
+import org.finos.legend.pure.runtime.java.compiled.execution.FunctionExecutionCompiledBuilder;
+import org.finos.legend.pure.runtime.java.compiled.factory.JavaModelFactoryRegistryLoader;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestColSpecAnnotations extends AbstractPureTestWithCoreCompiled
+{
+    @BeforeClass
+    public static void setUp()
+    {
+        setUpRuntime(new FunctionExecutionCompiledBuilder().build(), JavaModelFactoryRegistryLoader.loader());
+    }
+
+    @After
+    public void cleanRuntime()
+    {
+        runtime.delete("testSource.pure");
+        runtime.compile();
+    }
+
+    @Test
+    public void testSimpleColumnWithStereotype()
+    {
+        compileTestSource("testSource.pure",
+                "Profile test::myProfile\n" +
+                        "{\n" +
+                        "   stereotypes: [ignore];\n" +
+                        "   tags: [enumValue, propertyName];\n" +
+                        "}\n" +
+                "function go():Any[*]\n" +
+                        "{\n" +
+                        "   let colSpec = getColSpec();\n" +
+                        "   assertEquals(1, $colSpec.stereotypes->length());\n" +
+                        "}\n" +
+                        "function getColSpec():meta::pure::metamodel::relation::ColSpec<(firstName:String)>[1]\n" +
+                        "{\n" +
+                        "   ~<<test::myProfile.ignore>> firstName:String[1];\n" +
+                        "}\n"
+        );
+        execute("go():Any[*]");
+    }
+}
