ab31da4b721e6c957c4d6d593413f27b00270e94
Arctaedius, Erland M [Engineering] <erland.arctaedius@ny.email.gs.com>
2026-02-09 14:13:39 +0000

    Manage classloaders explicitly in our mojos

--- legend-pure-maven/legend-pure-maven-generation-par/src/main/java/org/finos/legend/pure/maven/par/PureJarMojo.java
+++ legend-pure-maven/legend-pure-maven-generation-par/src/main/java/org/finos/legend/pure/maven/par/PureJarMojo.java
@@ -15,16 +15,32 @@
 package org.finos.legend.pure.maven.par;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.DependencyResolutionException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectDependenciesResolver;
+import org.eclipse.aether.RepositorySystemSession;
 import org.finos.legend.pure.m3.generator.Log;
 import org.finos.legend.pure.m3.generator.par.PureJarGenerator;
+import org.finos.legend.pure.maven.shared.DependencyResolutionScope;
+import org.finos.legend.pure.maven.shared.ProjectDependencyResolution;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Set;
 
-@Mojo(name = "build-pure-jar", threadSafe = true)
+@Mojo(
+        name = "build-pure-jar",
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.TEST
+)
 public class PureJarMojo extends AbstractMojo
 {
     @Parameter
@@ -48,8 +64,75 @@ public class PureJarMojo extends AbstractMojo
     @Parameter
     private Set<String> extraRepositories;
 
+    /**
+     * <p>The scope of the dependencies to resolve from the Maven module. Use names from {@link DependencyResolutionScope}.
+     * If not specified, defaults to
+     * <ul>
+     *     <li>{@code test} if the current execution phase is a "test phase"
+     *     (see {@link ProjectDependencyResolution#inTestPhase(MojoExecution)}), or</li>
+     *     <li>{@code compile} otherwise.</li>
+     * </ul></p>
+     *
+     * @see DependencyResolutionScope
+     */
+    @Parameter
+    protected String dependencyScope;
+
+    @Parameter(readonly = true, required = true, defaultValue = "${project.build.outputDirectory}")
+    private File projectOutputDirectory;
+
+    @Parameter(readonly = true, required = true, defaultValue = "${project.build.testOutputDirectory}")
+    private File projectTestOutputDirectory;
+
+    @Parameter(readonly = true, required = true, defaultValue = "${mojoExecution}")
+    private MojoExecution mojoExecution;
+
+    @Parameter(readonly = true, required = true, defaultValue = "${repositorySystemSession}")
+    private RepositorySystemSession mavenRepoSession;
+
+    @Parameter(readonly = true, required = true, defaultValue = "${project}")
+    private MavenProject mavenProject;
+
+    @Component
+    private ProjectDependenciesResolver mavenProjectDependenciesResolver;
+
     @Override
     public void execute() throws MojoExecutionException
+    {
+        ClassLoader savedClassLoader = Thread.currentThread().getContextClassLoader();
+        try
+        {
+            DependencyResolutionScope dependencyResolutionScope = ProjectDependencyResolution.determineDependencyResolutionScope(dependencyScope, mojoExecution);
+            URL[] dependencyUrls = ProjectDependencyResolution.getDependencyURLs(
+                    dependencyResolutionScope,
+                    mavenProject,
+                    mojoExecution,
+                    mavenRepoSession,
+                    projectOutputDirectory,
+                    projectTestOutputDirectory,
+                    mavenProjectDependenciesResolver
+            );
+            try (URLClassLoader classLoader = new URLClassLoader(dependencyUrls, Thread.currentThread().getContextClassLoader()))
+            {
+                Thread.currentThread().setContextClassLoader(classLoader);
+                this.executeWithinClassLoader(classLoader);
+            }
+            catch (IOException e)
+            {
+                throw new MojoExecutionException("Error closing classloader", e);
+            }
+            finally
+            {
+                Thread.currentThread().setContextClassLoader(savedClassLoader);
+            }
+        }
+        catch (DependencyResolutionException e)
+        {
+            throw new MojoExecutionException("Error setting up classloader with project dependencies", e);
+        }
+    }
+
+    private void executeWithinClassLoader(ClassLoader classLoader) throws MojoExecutionException
     {
         try
         {
@@ -60,6 +143,7 @@ public class PureJarMojo extends AbstractMojo
                     this.modelVersion,
                     this.sourceDirectory,
                     this.outputDirectory,
+                    classLoader,
                     new Log()
                     {
                         @Override
@@ -88,6 +172,4 @@ public class PureJarMojo extends AbstractMojo
             throw new MojoExecutionException((eMessage == null) ? baseMessage : (baseMessage + ": " + eMessage), e);
         }
     }
-
-
 }
