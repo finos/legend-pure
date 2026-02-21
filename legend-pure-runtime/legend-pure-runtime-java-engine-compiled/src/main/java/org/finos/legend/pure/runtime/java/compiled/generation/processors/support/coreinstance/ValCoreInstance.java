f3d8aa8bdd63fdeec577ef544416c21f286c058a
Kevin Knight <57677197+kevin-m-knight-gs@users.noreply.github.com>
2026-02-20 20:16:25 -0500

    Make ValCoreInstance implement PrimitiveCoreInstance (#1198)

--- legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/main/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/support/coreinstance/ValCoreInstance.java
+++ legend-pure-runtime/legend-pure-runtime-java-engine-compiled/src/main/java/org/finos/legend/pure/runtime/java/compiled/generation/processors/support/coreinstance/ValCoreInstance.java
@@ -23,9 +23,18 @@ import org.finos.legend.pure.m4.ModelRepository;
 import org.finos.legend.pure.m4.coreinstance.CoreInstance;
 import org.finos.legend.pure.m4.coreinstance.SourceInformation;
 import org.finos.legend.pure.m4.coreinstance.indexing.IndexSpecification;
+import org.finos.legend.pure.m4.coreinstance.primitive.BooleanCoreInstance;
+import org.finos.legend.pure.m4.coreinstance.primitive.ByteCoreInstance;
+import org.finos.legend.pure.m4.coreinstance.primitive.DateCoreInstance;
+import org.finos.legend.pure.m4.coreinstance.primitive.DecimalCoreInstance;
+import org.finos.legend.pure.m4.coreinstance.primitive.PrimitiveCoreInstance;
+import org.finos.legend.pure.m4.coreinstance.primitive.StrictTimeCoreInstance;
+import org.finos.legend.pure.m4.coreinstance.primitive.StringCoreInstance;
 import org.finos.legend.pure.m4.coreinstance.primitive.date.DateFunctions;
 import org.finos.legend.pure.m4.coreinstance.primitive.date.LatestDate;
 import org.finos.legend.pure.m4.coreinstance.primitive.date.PureDate;
+import org.finos.legend.pure.m4.coreinstance.primitive.strictTime.PureStrictTime;
+import org.finos.legend.pure.m4.coreinstance.primitive.strictTime.StrictTimeFunctions;
 import org.finos.legend.pure.m4.exception.PureCompilationException;
 import org.finos.legend.pure.m4.tools.SafeAppendable;
 import org.finos.legend.pure.m4.transaction.ModelRepositoryTransaction;
@@ -35,91 +44,48 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Objects;
 
-public class ValCoreInstance extends AbstractCompiledCoreInstance
+public abstract class ValCoreInstance<T> extends AbstractCompiledCoreInstance implements PrimitiveCoreInstance<T>
 {
-    private final String val;
-    private final String type;
+    private final T val;
 
-    public ValCoreInstance(String val, String type)
+    private ValCoreInstance(T val)
     {
         this.val = val;
-        this.type = type;
     }
 
     @Override
     public boolean equals(Object o)
     {
+        if (this == o)
+        {
+            return true;
+        }
         if (!(o instanceof ValCoreInstance))
         {
             return false;
         }
-        ValCoreInstance that = (ValCoreInstance) o;
-        return Objects.equals(val, that.val) && Objects.equals(type, that.type);
+        ValCoreInstance<?> that = (ValCoreInstance<?>) o;
+        return Objects.equals(this.val, that.val) && Objects.equals(getType(), that.getType());
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hash(super.hashCode(), val, type);
+        return Objects.hash(super.hashCode(), this.val, getType());
     }
 
-    public Object getValue()
+    public T getValue()
     {
-        switch (this.type)
-        {
-            case M3Paths.Integer:
-            {
-                return Long.valueOf(this.val);
-            }
-            case M3Paths.String:
-            {
-                return this.val;
-            }
-            case M3Paths.Boolean:
-            {
-                return ModelRepository.getBooleanValue(this.val);
-            }
-            case M3Paths.Date:
-            case M3Paths.StrictDate:
-            case M3Paths.DateTime:
-            {
-                return DateFunctions.parsePureDate(this.val);
-            }
-            case M3Paths.LatestDate:
-            {
-                return LatestDate.instance;
-            }
-            case M3Paths.Float:
-            {
-                return Double.valueOf(this.val);
-            }
-            case M3Paths.Decimal:
-            {
-                return new BigDecimal(this.val);
-            }
-            default:
-            {
-                throw new RuntimeException(this.type + " is not supported yet");
-            }
-        }
+        return this.val;
     }
 
     @Override
     public String toString()
     {
-        return this.val + " instanceOf " + this.type;
+        return getName() + " instanceOf " + getType();
     }
 
-    public String getType()
-    {
-        return this.type;
-    }
-
-    @Override
-    public String getName()
-    {
-        return this.val;
-    }
+    public abstract String getType();
 
     @Override
     public ModelRepository getRepository()
@@ -235,12 +201,6 @@ public class ValCoreInstance extends AbstractCompiledCoreInstance
     }
 
     @Override
-    public CoreInstance copy()
-    {
-        return new ValCoreInstance(this.val, this.type);
-    }
-
-    @Override
     public void print(Appendable appendable, String tab, int max)
     {
         SafeAppendable.wrap(appendable).append(tab).append(this.val);
@@ -265,7 +225,57 @@ public class ValCoreInstance extends AbstractCompiledCoreInstance
 
     public static CoreInstance newVal(String val, String type)
     {
-        return new ValCoreInstance(val, type);
+        switch (type)
+        {
+            case M3Paths.Boolean:
+            {
+                return new BooleanVal(ModelRepository.getBooleanValue(val));
+            }
+            case M3Paths.Byte:
+            {
+                return new ByteVal(Byte.parseByte(val));
+            }
+            case M3Paths.Date:
+            {
+                return new DateVal(DateFunctions.parsePureDate(val));
+            }
+            case M3Paths.DateTime:
+            {
+                return new DateTimeVal(DateFunctions.parsePureDate(val));
+            }
+            case M3Paths.Decimal:
+            {
+                return new DecimalVal(new BigDecimal(val));
+            }
+            case M3Paths.Float:
+            {
+                return new FloatVal(Double.valueOf(val));
+            }
+            case M3Paths.Integer:
+            {
+                return new IntegerVal(Long.valueOf(val));
+            }
+            case M3Paths.LatestDate:
+            {
+                return new LatestDateVal();
+            }
+            case M3Paths.StrictDate:
+            {
+                return new StrictDateVal(DateFunctions.parsePureDate(val));
+            }
+            case M3Paths.StrictTime:
+            {
+                return new StrictTimeVal(StrictTimeFunctions.parsePureStrictTime(val));
+            }
+            case M3Paths.String:
+            {
+                return new StringVal(val);
+            }
+            default:
+            {
+                throw new RuntimeException(type + " is not supported yet");
+            }
+        }
     }
 
     public static CoreInstance toCoreInstance(Object value)
@@ -278,40 +288,353 @@ public class ValCoreInstance extends AbstractCompiledCoreInstance
         {
             return (CoreInstance) value;
         }
-        String type;
         if (value instanceof Boolean)
         {
-            type = M3Paths.Boolean;
+            return new BooleanVal((Boolean) value);
         }
-        else if (value instanceof PureDate)
+        if (value instanceof Byte)
         {
-            type = DateFunctions.datePrimitiveType((PureDate) value);
+            return new ByteVal((Byte) value);
         }
-        else if ((value instanceof Long) || (value instanceof Integer) || (value instanceof BigInteger))
+        if (value instanceof PureDate)
         {
-            type = M3Paths.Integer;
+            PureDate date = (PureDate) value;
+            String type = DateFunctions.datePrimitiveType(date);
+            switch (type)
+            {
+                case M3Paths.DateTime:
+                {
+                    return new DateTimeVal(date);
+                }
+                case M3Paths.StrictDate:
+                {
+                    return new StrictDateVal(date);
+                }
+                case M3Paths.LatestDate:
+                {
+                    return new LatestDateVal();
+                }
+                case M3Paths.Date:
+                {
+                    return new DateVal(date);
+                }
+                default:
+                {
+                    throw new RuntimeException("Unknown date type '" + type + "' for date: " + date);
+                }
+            }
         }
-        else if ((value instanceof Double) || (value instanceof Float))
+        if ((value instanceof Long) || (value instanceof Integer) || (value instanceof BigInteger))
         {
-            type = M3Paths.Float;
+            return new IntegerVal(((Number) value).longValue());
         }
-        else if (value instanceof BigDecimal)
+        if ((value instanceof Double) || (value instanceof Float))
         {
-            type = M3Paths.Decimal;
+            return new FloatVal(((Number) value).doubleValue());
         }
-        else if (value instanceof String)
+        if (value instanceof BigDecimal)
         {
-            type = M3Paths.String;
+            return new DecimalVal((BigDecimal) value);
         }
-        else
+        if (value instanceof String)
         {
-            throw new RuntimeException("TODO " + value.getClass());
+            return new StringVal((String) value);
         }
-        return new ValCoreInstance(CompiledSupport.primitiveToString(value), type);
+        if (value instanceof PureStrictTime)
+        {
+            return new StrictTimeVal((PureStrictTime) value);
+        }
+        throw new RuntimeException("TODO " + value.getClass());
     }
 
     public static ListIterable<CoreInstance> toCoreInstances(RichIterable<?> values)
     {
         return ((values == null) || values.isEmpty()) ? Lists.immutable.empty() : values.collect(ValCoreInstance::toCoreInstance, Lists.mutable.ofInitialCapacity(values.size()));
+    }
+
+    private static class BooleanVal extends ValCoreInstance<Boolean> implements BooleanCoreInstance
+    {
+        private BooleanVal(boolean value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.Boolean;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue().booleanValue());
+        }
+
+        @Override
+        public BooleanCoreInstance copy()
+        {
+            return new BooleanVal(getValue());
+        }
+    }
+
+    private static class ByteVal extends ValCoreInstance<Byte> implements ByteCoreInstance
+    {
+        private ByteVal(byte value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.Byte;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue().byteValue());
+        }
+
+        @Override
+        public ByteCoreInstance copy()
+        {
+            return new ByteVal(getValue());
+        }
+    }
+
+    private static class DateVal extends ValCoreInstance<PureDate> implements DateCoreInstance
+    {
+        private DateVal(PureDate value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.Date;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public DateCoreInstance copy()
+        {
+            return new DateVal(getValue());
+        }
+    }
+
+    private static class StrictDateVal extends ValCoreInstance<PureDate> implements DateCoreInstance
+    {
+        private StrictDateVal(PureDate value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.StrictDate;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public DateCoreInstance copy()
+        {
+            return new StrictDateVal(getValue());
+        }
+    }
+
+    private static class DateTimeVal extends ValCoreInstance<PureDate> implements DateCoreInstance
+    {
+        private DateTimeVal(PureDate value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.DateTime;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public DateCoreInstance copy()
+        {
+            return new DateTimeVal(getValue());
+        }
+    }
+
+    private static class LatestDateVal extends ValCoreInstance<PureDate> implements DateCoreInstance
+    {
+        private LatestDateVal()
+        {
+            super(LatestDate.instance);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.LatestDate;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public DateCoreInstance copy()
+        {
+            return new LatestDateVal();
+        }
+    }
+
+    private static class IntegerVal extends ValCoreInstance<Long>
+    {
+        private IntegerVal(Long value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.Integer;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public PrimitiveCoreInstance<Long> copy()
+        {
+            return new IntegerVal(getValue());
+        }
+    }
+
+    private static class FloatVal extends ValCoreInstance<Double>
+    {
+        private FloatVal(Double value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.Float;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue().doubleValue());
+        }
+
+        @Override
+        public PrimitiveCoreInstance<Double> copy()
+        {
+            return new FloatVal(getValue());
+        }
+    }
+
+    private static class DecimalVal extends ValCoreInstance<BigDecimal> implements DecimalCoreInstance
+    {
+        private DecimalVal(BigDecimal value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.Decimal;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public DecimalCoreInstance copy()
+        {
+            return new DecimalVal(getValue());
+        }
+    }
+
+    private static class StringVal extends ValCoreInstance<String> implements StringCoreInstance
+    {
+        private StringVal(String value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.String;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public StringCoreInstance copy()
+        {
+            return new StringVal(getValue());
+        }
+    }
+
+    private static class StrictTimeVal extends ValCoreInstance<PureStrictTime> implements StrictTimeCoreInstance
+    {
+        private StrictTimeVal(PureStrictTime value)
+        {
+            super(value);
+        }
+
+        @Override
+        public String getType()
+        {
+            return M3Paths.StrictTime;
+        }
+
+        @Override
+        public String getName()
+        {
+            return CompiledSupport.primitiveToString(getValue());
+        }
+
+        @Override
+        public StrictTimeCoreInstance copy()
+        {
+            return new StrictTimeVal(getValue());
+        }
     }
 }
