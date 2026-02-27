9a592b087aa3efb498ba5db26afe1f4696817991
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2026-02-27 12:11:58 -0500

    Revert "refactor: Make separate function names for compiled vs. interpreted"
    This reverts commit 05b4cae9b0eb4a05475467208b27e4410d9503ae.

--- legend-pure-store/legend-pure-store-relational/legend-pure-m2-store-relational-grammar/src/main/java/org/finos/legend/pure/m2/relational/serialization/grammar/v1/RelationalParser.java
+++ legend-pure-store/legend-pure-store-relational/legend-pure-m2-store-relational-grammar/src/main/java/org/finos/legend/pure/m2/relational/serialization/grammar/v1/RelationalParser.java
@@ -308,12 +308,13 @@ public class RelationalParser implements IRelationalParser
         return Tuples.pair(
                 table,
                 _RelationType.build(
-                        table._columns().collect(c -> (CoreInstance) _Column.getColumnInstanceInterpreted(
+                        table._columns().collect(c -> (CoreInstance) _Column.getColumnInstance(
                                         c.getValueForMetaPropertyToOne("name").getName(),
                                         false,
                                         convertType(c.getValueForMetaPropertyToOne("type"), repository, processorSupport),
                                         (Multiplicity) org.finos.legend.pure.m3.navigation.multiplicity.Multiplicity.newMultiplicity(((Column) c)._nullable() ? 0 : 1, 1, processorSupport),
                                         ((Column) c)._stereotypesCoreInstance(),
+                                        false,
                                         ((Column) c)._taggedValues(),
                                         sourceInformation,
                                         processorSupport
