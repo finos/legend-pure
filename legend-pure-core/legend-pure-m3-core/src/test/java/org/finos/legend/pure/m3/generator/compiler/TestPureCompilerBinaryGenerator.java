4182426fd2607ff929bd6b3cd8bc2fe96a04dffb
kevin-m-knight-gs <kevin.knight@gs.com>
2026-03-09 10:01:24 -0400

    Add more tests for PureCompilerBinaryGenerator

--- legend-pure-core/legend-pure-m3-core/src/test/java/org/finos/legend/pure/m3/generator/compiler/TestPureCompilerBinaryGenerator.java
+++ legend-pure-core/legend-pure-m3-core/src/test/java/org/finos/legend/pure/m3/generator/compiler/TestPureCompilerBinaryGenerator.java
@@ -15,6 +15,9 @@
 package org.finos.legend.pure.m3.generator.compiler;
 
 import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Sets;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.set.MutableSet;
 import org.finos.legend.pure.m3.serialization.compiler.element.ConcreteElementDeserializer;
 import org.finos.legend.pure.m3.serialization.compiler.file.FileDeserializer;
 import org.finos.legend.pure.m3.serialization.compiler.file.FilePathProvider;
@@ -26,7 +29,11 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class TestPureCompilerBinaryGenerator
 {
@@ -52,25 +59,26 @@ public class TestPureCompilerBinaryGenerator
     @Test
     public void testSerializeAllModules() throws IOException
     {
-        Path outputDirectory = TMP.newFolder().toPath();
-        PureCompilerBinaryGenerator.serializeModules(outputDirectory, null);
-
         FileDeserializer deserializer = newFileDeserializer();
-        assertModuleSerialized(deserializer, outputDirectory, PLATFORM);
-        assertModuleSerialized(deserializer, outputDirectory, TEST_REPO);
-        assertModuleSerialized(deserializer, outputDirectory, OTHER_TEST_REPO);
-    }
 
-    @Test
-    public void testSerializeAllModulesExplicitlyRequested() throws IOException
-    {
-        Path outputDirectory = TMP.newFolder().toPath();
-        PureCompilerBinaryGenerator.serializeModules(outputDirectory, Lists.immutable.with(PLATFORM, TEST_REPO, OTHER_TEST_REPO));
+        // Check the case where all modules are implicitly specified and serialized individually
+        Path implicitOutputDir = TMP.newFolder().toPath();
+        PureCompilerBinaryGenerator.serializeModules(implicitOutputDir, null, true);
 
-        FileDeserializer deserializer = newFileDeserializer();
-        assertModuleSerialized(deserializer, outputDirectory, PLATFORM);
-        assertModuleSerialized(deserializer, outputDirectory, TEST_REPO);
-        assertModuleSerialized(deserializer, outputDirectory, OTHER_TEST_REPO);
+        assertModuleSerialized(deserializer, implicitOutputDir, PLATFORM);
+        assertModuleSerialized(deserializer, implicitOutputDir, TEST_REPO);
+        assertModuleSerialized(deserializer, implicitOutputDir, OTHER_TEST_REPO);
+
+        // Check the case where all modules are explicitly specified and serialized together
+        Path explicitOutputDir = TMP.newFolder().toPath();
+        PureCompilerBinaryGenerator.serializeModules(explicitOutputDir, Lists.immutable.with(PLATFORM, TEST_REPO, OTHER_TEST_REPO), false);
+
+        assertModuleSerialized(deserializer, explicitOutputDir, PLATFORM);
+        assertModuleSerialized(deserializer, explicitOutputDir, TEST_REPO);
+        assertModuleSerialized(deserializer, explicitOutputDir, OTHER_TEST_REPO);
+
+        // Assert that the directories have identical content
+        assertDirectoriesEquivalent(implicitOutputDir, explicitOutputDir);
     }
 
     @Test
@@ -112,6 +120,59 @@ public class TestPureCompilerBinaryGenerator
     private static void assertModuleNotSerialized(FileDeserializer deserializer, Path outputDirectory, String moduleName)
     {
         Assert.assertFalse(moduleName + " manifest should not exist", deserializer.moduleManifestExists(outputDirectory, moduleName));
+    }
+
+    private static void assertDirectoriesEquivalent(Path dir1, Path dir2)
+    {
+        // First check that relative files paths are the same
+        MutableSet<Path> paths1 = Sets.mutable.empty();
+        MutableSet<Path> paths2 = Sets.mutable.empty();
+        try (Stream<Path> stream1 = Files.walk(dir1);
+             Stream<Path> stream2 = Files.walk(dir2))
+        {
+            stream1.map(dir1::relativize).forEach(paths1::add);
+            stream2.map(dir2::relativize).forEach(paths2::add);
+        }
+        catch (IOException e)
+        {
+            throw new UncheckedIOException(e);
+        }
+        Assert.assertEquals(paths1, paths2);
+
+        MutableList<Path> mismatchFiles = Lists.mutable.empty();
+        paths1.forEach(path ->
+        {
+            Path file1 = dir1.resolve(path);
+            Path file2 = dir2.resolve(path);
+            if (Files.isDirectory(file1))
+            {
+                if (!Files.isDirectory(file2))
+                {
+                    mismatchFiles.add(path);
+                }
+            }
+            else if (Files.isDirectory(file2))
+            {
+                mismatchFiles.add(path);
+            }
+            else
+            {
+                try
+                {
+                    byte[] bytes1 = Files.readAllBytes(file1);
+                    byte[] bytes2 = Files.readAllBytes(file2);
+                    if (!Arrays.equals(bytes1, bytes2))
+                    {
+                        mismatchFiles.add(path);
+                    }
+                }
+                catch (IOException e)
+                {
+                    throw new UncheckedIOException(e);
+                }
+            }
+        });
+        Assert.assertEquals(Lists.fixedSize.empty(), mismatchFiles);
     }
 
     private static FileDeserializer newFileDeserializer()
