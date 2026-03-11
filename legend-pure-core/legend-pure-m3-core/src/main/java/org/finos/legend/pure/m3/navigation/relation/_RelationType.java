5de1c9263391fac94069f87f4b22dd28d1858808
Travis Stebbins <travis.stebbins@ny.email.gs.com>
2026-03-11 09:23:54 -0700

    Add support for annotations for Columns and ColSpecs (#1190)
    * Add support for stereotypes and tagged values to Columns/ColSpecs
    
    * fix: Fix setting stereotypesCoreInstance
    
    * feat: Add colSpec function that takes Column param
    
    * feat: Add new function signature to FunctionExpressionProcessor
    
    * feat: Add TestColSpecAnnotations
    
    * feat: Call new colSpec method if column has stereotypes/tagged values
    
    * feat: Add tests for col spec
    
    * feat: Add test to TestColSpecAnnotations
    
    * feat: Try not overloading the colSpec function
    
    * refactor: Make separate generateStereotypesBuilder and generateTaggedValuesBuilder functions
    
    * feat: Update TestColSpecAnnotations
    
    * fix: Fix processing tagged values in _Column
    
    * feat: Add tests to TestColSpecAnnotations
    
    * feat: Add overloaded getColumnInstance function that takes string type
    
    * feat: Remove extends AnnotatedElement from ColSpec class
    
    * fix: Remove unused import
    
    * feat: Feedback from comments
    
    * refactor: Update to getUserPathForPackageableElement
    
    * fix: Resolve import stubs in _RelationType
    
    * feat: Update getColumnInstance functions to handle resolved import stubs
    
    * fix: Fix paths in GenericTypeSerializationInCode
    
    * fix: Set assumeNoImportStubs flag to true
    
    * fix: Uncomment tests
    
    * fix: Update GenericType functions to pass stereotypes and tagged values
    
    * fix: Propagate stereotypes/tagged values wherever _Column.getColumnInstance is used
    
    * fix: Ensure correct passing of _stereotypes()/_stereotypesCoreInstance()
    
    * refactor: Re-add overloaded function calls
    
    * refactor: Add docs to getColumnInstance
    
    * fix: Add missing package
    
    * fix: Change to using _stereotypes() in GenericType
    
    * fix: Remove stereotypes/taggeed value passing in TDSExtension
    
    * refactor: Make separate function names for compiled vs. interpreted
    
    * Revert "refactor: Make separate function names for compiled vs. interpreted"
    
    This reverts commit 05b4cae9b0eb4a05475467208b27e4410d9503ae.
    
    * refactor: Remove assumeNoImportStubs param
    
    * fix: Merge stereotypes/tagged values in merge function
    
    * fix: Ensure only distinct stereotypes/tagged values
    
    * fix: Use _stereotypes instead of _stereotypesCoreInstance in merge function

--- legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/navigation/relation/_RelationType.java
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/navigation/relation/_RelationType.java
@@ -24,6 +24,8 @@ import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.set.MutableSet;
 import org.eclipse.collections.api.tuple.Pair;
 import org.eclipse.collections.impl.tuple.Tuples;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.extension.Stereotype;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.extension.TaggedValue;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.Function;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.FunctionAccessor;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.relation.Column;
@@ -225,7 +227,13 @@ public class _RelationType
                             Type rawTypeB = (Type) Instance.getValueForMetaPropertyToOneResolved(b, M3Properties.rawType, processorSupport);
                             GenericType merged = rawTypeA == null ? b : rawTypeB == null ? a : (GenericType) org.finos.legend.pure.m3.navigation.generictype.GenericType.findBestCommonGenericType(Lists.mutable.with(a, b), isCovariant, false, genericTypeCopy.getSourceInformation(), processorSupport);
                             org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.multiplicity.Multiplicity mergedMul = rawTypeA == null ? _Column.getColumnMultiplicity(c.getTwo()) : rawTypeB == null ? _Column.getColumnMultiplicity(c.getOne()) : (org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.multiplicity.Multiplicity) Multiplicity.minSubsumingMultiplicity(_Column.getColumnMultiplicity(c.getOne()), _Column.getColumnMultiplicity(c.getTwo()), processorSupport);
-                            return _Column.getColumnInstance(cName, wildcard, merged, mergedMul, null, processorSupport);
+                            RichIterable<? extends Stereotype> stereotypesA = c.getOne()._stereotypes() == null ? Lists.mutable.empty() : c.getOne()._stereotypes();
+                            RichIterable<? extends Stereotype> stereotypesB = c.getTwo()._stereotypes() == null ? Lists.mutable.empty() : c.getTwo()._stereotypes();
+                            RichIterable<? extends Stereotype> mergedStereotypes = Lists.mutable.<Stereotype>withAll(stereotypesA).withAll(stereotypesB).distinct();
+                            RichIterable<? extends TaggedValue> taggedValuesA = c.getOne()._taggedValues() == null ? Lists.mutable.empty() : c.getOne()._taggedValues();
+                            RichIterable<? extends TaggedValue> taggedValuesB = c.getTwo()._taggedValues() == null ? Lists.mutable.empty() : c.getTwo()._taggedValues();
+                            RichIterable<? extends TaggedValue> mergedTaggedValues = Lists.mutable.<TaggedValue>withAll(taggedValuesA).withAll(taggedValuesB).distinctBy(TaggedValue::_tagCoreInstance);
+                            return _Column.getColumnInstance(cName, wildcard, merged, mergedMul, mergedStereotypes, mergedTaggedValues, null, processorSupport);
                         }),
                         existingGenericType.getValueForMetaPropertyToOne(M3Properties.rawType).getSourceInformation(),
                         processorSupport
@@ -256,6 +264,8 @@ public class _RelationType
     {
         relationType.getValueForMetaPropertyToMany(M3Properties.columns).forEach(c ->
         {
+            c.getValueForMetaPropertyToMany(M3Properties.stereotypes).forEach(s -> ImportStub.withImportStubByPass(s, processorSupport));
+            c.getValueForMetaPropertyToMany(M3Properties.taggedValues).forEach(tv -> ImportStub.withImportStubByPass(tv.getValueForMetaPropertyToOne(M3Properties.tag), processorSupport));
             CoreInstance classifierGenericType = c.getValueForMetaPropertyToOne(M3Properties.classifierGenericType);
             classifierGenericType.getValueForMetaPropertyToMany(M3Properties.multiplicityArguments).forEach(arg -> ImportStub.withImportStubByPass(arg, processorSupport));
             // the first type argument of the column type is the relation type itself: we skip it to avoid infinite recursion
