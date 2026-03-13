c89f254e02d689ffcbdf4c75dae9fb92b66d7d88
kevin-m-knight-gs <kevin.knight@gs.com>
2026-03-13 11:47:51 -0400

    Add a new concrete element serializer version that compresses its data

---
+++ legend-pure-core/legend-pure-m3-core/src/main/java/org/finos/legend/pure/m3/serialization/compiler/element/v2/ConcreteElementSerializerV2.java
@@ -0,0 +1,77 @@
+// Copyright 2026 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.pure.m3.serialization.compiler.element.v2;
+
+import org.finos.legend.pure.m3.navigation.ProcessorSupport;
+import org.finos.legend.pure.m3.serialization.compiler.element.ConcreteElementSerializerExtension;
+import org.finos.legend.pure.m3.serialization.compiler.element.DeserializedConcreteElement;
+import org.finos.legend.pure.m3.serialization.compiler.element.v1.ConcreteElementSerializerV1;
+import org.finos.legend.pure.m3.serialization.compiler.reference.ReferenceIdProvider;
+import org.finos.legend.pure.m3.serialization.compiler.strings.StringIndexer;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+public class ConcreteElementSerializerV2 implements ConcreteElementSerializerExtension
+{
+    private final ConcreteElementSerializerV1 v1 = new ConcreteElementSerializerV1();
+
+    @Override
+    public int version()
+    {
+        return 2;
+    }
+
+    @Override
+    public void serialize(OutputStream stream, CoreInstance element, StringIndexer stringIndexer, ReferenceIdProvider referenceIdProvider, ProcessorSupport processorSupport)
+    {
+        Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION, true);
+        try
+        {
+            DeflaterOutputStream zipStream = new DeflaterOutputStream(stream, deflater);
+            this.v1.serialize(zipStream, element, stringIndexer, referenceIdProvider, processorSupport);
+            zipStream.finish();
+        }
+        catch (IOException e)
+        {
+            throw new UncheckedIOException(e);
+        }
+        finally
+        {
+            deflater.end();
+        }
+    }
+
+    @Override
+    public DeserializedConcreteElement deserialize(InputStream stream, StringIndexer stringIndexer, int referenceIdVersion)
+    {
+        Inflater inflater = new Inflater(true);
+        try
+        {
+            return this.v1.deserialize(new InflaterInputStream(stream, inflater), stringIndexer, referenceIdVersion);
+        }
+        finally
+        {
+            inflater.end();
+        }
+    }
+}
