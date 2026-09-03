3dd5851679652b3433e3e23e768fdd7505eec796
kevin-m-knight-gs <kevin.knight@gs.com>
2026-09-03 18:36:52 -0400

    Minor improvement to ResultSetValueHandlers

--- legend-pure-store/legend-pure-store-relational/legend-pure-runtime-java-extension-compiled-store-relational/src/main/java/org/finos/legend/pure/runtime/java/extension/store/relational/compiled/natives/ResultSetValueHandlers.java
+++ legend-pure-store/legend-pure-store-relational/legend-pure-runtime-java-extension-compiled-store-relational/src/main/java/org/finos/legend/pure/runtime/java/extension/store/relational/compiled/natives/ResultSetValueHandlers.java
@@ -14,12 +14,13 @@
 
 package org.finos.legend.pure.runtime.java.extension.store.relational.compiled.natives;
 
-import org.eclipse.collections.api.factory.Stacks;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Maps;
 import org.eclipse.collections.api.list.ListIterable;
 import org.eclipse.collections.api.list.MutableList;
-import org.eclipse.collections.api.map.primitive.MutableIntObjectMap;
-import org.eclipse.collections.impl.factory.Lists;
-import org.eclipse.collections.impl.map.mutable.primitive.IntObjectHashMap;
+import org.eclipse.collections.api.map.ImmutableMap;
+import org.eclipse.collections.api.map.primitive.ImmutableIntObjectMap;
+import org.eclipse.collections.impl.factory.primitive.IntObjectMaps;
 import org.finos.legend.pure.m3.exception.PureExecutionException;
 import org.finos.legend.pure.m3.tools.BinaryUtils;
 import org.finos.legend.pure.m4.coreinstance.CoreInstance;
@@ -34,141 +35,91 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.Calendar;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public final class ResultSetValueHandlers
 {
-    private static final ResultSetValueHandler NULL = new ResultSetValueHandler()
+    private static final ResultSetValueHandler NULL = (rs, i, nullSqlInstance, calendar) -> nullSqlInstance;
+
+    private static final ResultSetValueHandler STRING = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            return nullSqlInstance;
-        }
+        String str = rs.getString(i);
+        return str == null ? nullSqlInstance : str;
     };
 
-    private static final ResultSetValueHandler STRING = new ResultSetValueHandler()
+    private static final ResultSetValueHandler BOOLEAN = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            String str = rs.getString(i);
-            return str == null ? nullSqlInstance : str;
-        }
+        boolean bool = rs.getBoolean(i);
+        return rs.wasNull() ? nullSqlInstance : bool;
     };
 
-    private static final ResultSetValueHandler BOOLEAN = new ResultSetValueHandler()
+    private static final ResultSetValueHandler DATE = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            boolean bool = rs.getBoolean(i);
-            return rs.wasNull() ? nullSqlInstance : bool;
-        }
+        Date date = rs.getDate(i);
+        return date == null ? nullSqlInstance : StrictDate.fromSQLDate(date);
     };
 
-    private static final ResultSetValueHandler DATE = new ResultSetValueHandler()
+    private static final ResultSetValueHandler TIMESTAMP = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            Date date = rs.getDate(i);
-            return date == null ? nullSqlInstance : StrictDate.fromSQLDate(date);
-        }
+        Timestamp timestamp = rs.getTimestamp(i, calendar);
+        return timestamp == null ? nullSqlInstance : DateFunctions.fromSQLTimestamp(timestamp);
     };
 
-    private static final ResultSetValueHandler TIMESTAMP = new ResultSetValueHandler()
+    private static final ResultSetValueHandler LONG = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            Timestamp timestamp = rs.getTimestamp(i, calendar);
-            return timestamp == null ? nullSqlInstance : DateFunctions.fromSQLTimestamp(timestamp);
-        }
+        long val = rs.getLong(i);
+        return rs.wasNull() ? nullSqlInstance : val;
     };
 
-    private static final ResultSetValueHandler LONG = new ResultSetValueHandler()
+    private static final ResultSetValueHandler DOUBLE = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            long val = rs.getLong(i);
-            return rs.wasNull() ? nullSqlInstance : val;
-        }
+        double d = rs.getDouble(i);
+        return rs.wasNull() ? nullSqlInstance : d;
     };
 
-    private static final ResultSetValueHandler DOUBLE = new ResultSetValueHandler()
+    private static final ResultSetValueHandler DECIMAL = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            double d = rs.getDouble(i);
-            return rs.wasNull() ? nullSqlInstance : d;
-        }
+        BigDecimal bd = rs.getBigDecimal(i);
+        return bd == null ? nullSqlInstance : bd.doubleValue();
     };
 
-    private static final ResultSetValueHandler DECIMAL = new ResultSetValueHandler()
+    private static final ResultSetValueHandler BINARY = (rs, i, nullSqlInstance, calendar) ->
     {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            BigDecimal bd = rs.getBigDecimal(i);
-            return bd == null ? nullSqlInstance : bd.doubleValue();
-        }
+        byte[] bytes = rs.getBytes(i);
+        return bytes == null ? nullSqlInstance : BinaryUtils.encodeHex(bytes);
     };
 
-    private static final ResultSetValueHandler BINARY = new ResultSetValueHandler()
-    {
-        @Override
-        public Object value(ResultSet rs, int i, CoreInstance nullSqlInstance, Calendar calendar) throws SQLException
-        {
-            byte[] bytes = rs.getBytes(i);
-            return bytes == null ? nullSqlInstance : BinaryUtils.encodeHex(bytes);
-        }
-    };
-
-
-    private static final MutableIntObjectMap<ResultSetValueHandler> HANDLERS = IntObjectHashMap.newMap();
-    private static final Map<String, ResultSetValueHandler> DB_SPECIFIC_HANDLERS = new HashMap<>();
-
-    static
-    {
-        HANDLERS.put(Types.NULL, NULL);
-        HANDLERS.put(Types.BIT, BOOLEAN);
-        HANDLERS.put(Types.BOOLEAN, BOOLEAN);
-        HANDLERS.put(Types.DATE, DATE);
-        HANDLERS.put(Types.TIMESTAMP, TIMESTAMP);
-        HANDLERS.put(Types.TIMESTAMP_WITH_TIMEZONE, TIMESTAMP);
-        HANDLERS.put(Types.TINYINT, LONG);
-        HANDLERS.put(Types.SMALLINT, LONG);
-        HANDLERS.put(Types.INTEGER, LONG);
-        HANDLERS.put(Types.BIGINT, LONG);
-        HANDLERS.put(Types.FLOAT, DOUBLE);
-        HANDLERS.put(Types.REAL, DOUBLE);
-        HANDLERS.put(Types.DOUBLE, DOUBLE);
-        HANDLERS.put(Types.DECIMAL, DECIMAL);
-        HANDLERS.put(Types.NUMERIC, DECIMAL);
-        HANDLERS.put(Types.CHAR, STRING);
-        HANDLERS.put(Types.VARCHAR, STRING);
-        HANDLERS.put(Types.LONGVARCHAR, STRING);
-        HANDLERS.put(Types.NCHAR, STRING);
-        HANDLERS.put(Types.NVARCHAR, STRING);
-        HANDLERS.put(Types.LONGNVARCHAR, STRING);
-        HANDLERS.put(Types.OTHER, STRING);
-        HANDLERS.put(Types.BINARY, BINARY);
-        HANDLERS.put(Types.VARBINARY, BINARY);
-        HANDLERS.put(Types.LONGVARBINARY, BINARY);
-
-        DB_SPECIFIC_HANDLERS.put("HUGEINT", LONG); // DuckDb
-    }
-
+    private static final ImmutableIntObjectMap<ResultSetValueHandler> HANDLERS = IntObjectMaps.mutable.<ResultSetValueHandler>empty()
+            .withKeyValue(Types.NULL, NULL)
+            .withKeyValue(Types.BIT, BOOLEAN)
+            .withKeyValue(Types.BOOLEAN, BOOLEAN)
+            .withKeyValue(Types.DATE, DATE)
+            .withKeyValue(Types.TIMESTAMP, TIMESTAMP)
+            .withKeyValue(Types.TIMESTAMP_WITH_TIMEZONE, TIMESTAMP)
+            .withKeyValue(Types.TINYINT, LONG)
+            .withKeyValue(Types.SMALLINT, LONG)
+            .withKeyValue(Types.INTEGER, LONG)
+            .withKeyValue(Types.BIGINT, LONG)
+            .withKeyValue(Types.FLOAT, DOUBLE)
+            .withKeyValue(Types.REAL, DOUBLE)
+            .withKeyValue(Types.DOUBLE, DOUBLE)
+            .withKeyValue(Types.DECIMAL, DECIMAL)
+            .withKeyValue(Types.NUMERIC, DECIMAL)
+            .withKeyValue(Types.CHAR, STRING)
+            .withKeyValue(Types.VARCHAR, STRING)
+            .withKeyValue(Types.LONGVARCHAR, STRING)
+            .withKeyValue(Types.NCHAR, STRING)
+            .withKeyValue(Types.NVARCHAR, STRING)
+            .withKeyValue(Types.LONGNVARCHAR, STRING)
+            .withKeyValue(Types.OTHER, STRING)
+            .withKeyValue(Types.BINARY, BINARY)
+            .withKeyValue(Types.VARBINARY, BINARY)
+            .withKeyValue(Types.LONGVARBINARY, BINARY)
+            .toImmutable();
+    private static final ImmutableMap<String, ResultSetValueHandler> DB_SPECIFIC_HANDLERS = Maps.immutable.with("HUGEINT", LONG); // DuckDb
 
     private ResultSetValueHandlers()
     {
     }
-
 
     public interface ResultSetValueHandler
     {
@@ -177,24 +128,20 @@ public final class ResultSetValueHandlers
 
     public static ListIterable<ResultSetValueHandler> getHandlers(ResultSetMetaData metaData) throws SQLException
     {
-        MutableList<ResultSetValueHandler> handlers = Lists.mutable.of();
         int count = metaData.getColumnCount();
-
+        MutableList<ResultSetValueHandler> handlers = Lists.mutable.ofInitialCapacity(count);
         for (int i = 1; i <= count; i++)
         {
             int columnType = metaData.getColumnType(i);
             String columnTypeName = metaData.getColumnTypeName(i);
 
-            ResultSetValueHandler handler = DB_SPECIFIC_HANDLERS.getOrDefault(columnTypeName, HANDLERS.get(columnType));
-
+            ResultSetValueHandler handler = DB_SPECIFIC_HANDLERS.getIfAbsent(columnTypeName, () -> HANDLERS.get(columnType));
             if (handler == null)
             {
-                throw new PureExecutionException("Unhandled SQL data type (java.sql.Types): " + columnType + ", column: " + i + " " + metaData.getColumnName(i) + " " + columnTypeName, Stacks.mutable.empty());
+                throw new PureExecutionException("Unhandled SQL data type (java.sql.Types): " + columnType + ", column: " + i + " " + metaData.getColumnName(i) + " " + columnTypeName);
             }
-
             handlers.add(handler);
         }
-
         return handlers;
     }
 }
